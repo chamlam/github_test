7cd29b1f67db2681ff2288c80023a9362868ff66
David Lam <lamiokcham@gmail.com>
2022-06-26 16:33:31 +0800

    add log function

--- github_file_1.py
+++ github_file_1.py
@@ -1,5 +1,8 @@
+def log(msg):
+    print_things(msg)
 
 def print_things(x):
     print (x)
+
 if __name__ == "__main__":
     print_things("hello, this is the file created to be uploaded in github")
